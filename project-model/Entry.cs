47294cc187aa835def6cedaf2b14032f44bc1177
Plixelated <76276169+Plixelated@users.noreply.github.com>
2025-03-27 20:27:27 -0700

    added db and migrations

---
+++ project-model/Entry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using project_model;
+
+namespace project_model;
+
+[Table("Entry")]
+public partial class Entry
+{
+    [Key]
+    public int Id { get; set; }
+}
---
+++ project-model/ModelContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using Microsoft.EntityFrameworkCore;
+using project_model;
+
+namespace project_model;
+
+public class ModelContext : DbContext
+{
+    public DbSet<Entry> Entries { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=project_user;Database=submissions");
+}
