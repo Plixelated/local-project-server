53e5f50263106f8c216dd342cf6b0d70489c24b2
Plixelated <76276169+Plixelated@users.noreply.github.com>
2025-05-09 13:21:02 -0700

    more cors chores

--- project-server/Program.cs
+++ project-server/Program.cs
@@ -150,6 +150,17 @@ var app = builder.Build();
 
 string corsOrigin = string.Empty;
 
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method == HttpMethods.Options)
+    {
+        context.Response.StatusCode = 204;
+        return;
+    }
+
+    await next();
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
