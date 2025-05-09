cc21d031eb978826067843c4d6854ecd28b7c25a
Plixelated <76276169+Plixelated@users.noreply.github.com>
2025-05-09 15:31:48 -0700

    moved cors settings to dev

--- project-server/Program.cs
+++ project-server/Program.cs
@@ -156,18 +156,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
     corsOrigin = "http://localhost:4200";
+    app.UseCors(option =>
+    option.WithOrigins(corsOrigin)
+    .AllowAnyHeader()
+    .AllowCredentials()
+    .AllowAnyMethod()
+    );
 }
 else
 {
     corsOrigin = "https://stars.plixel.app";
 }
-
-app.UseCors(option => 
-option.WithOrigins(corsOrigin)
-.AllowAnyHeader()
-.AllowCredentials()
-.AllowAnyMethod()
-);
 
 app.UseHttpsRedirection();
 
